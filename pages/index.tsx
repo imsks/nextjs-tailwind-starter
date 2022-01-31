cc55ed518ceabf5345dc33f87f3cd6fbf3453e6f
Sachin Shukla <sachinkshuklaoo7@gmail.com>
2022-01-31 07:41:31 +0530

    project setup ready

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const IndexPage = () => {
+  return (
+    <div className="min-h-screen px-2 py-0 flex flex-col justify-center items-center">
+      Sachin
+    </div>
+  );
+};
+
+export default IndexPage;
